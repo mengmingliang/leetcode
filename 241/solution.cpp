c76c83e3ec6b689b86a5520034d2b3fadabbb276
mengmingliang <mml1106@126.com>
2018-07-18 00:12:59 -0400

    mike

---
+++ 241/solution.cpp
@@ -0,0 +1,33 @@
+class solution{
+	public:
+		vector<int> diffWaysToCompute(string input) {
+			        vector<int> ans;
+				        
+				        for(int i = 0;i < input.size(); ++i){
+						            if(input[i] == '+'||input[i] == '-'||input[i] == '*'){
+								                    auto left = diffWaysToCompute(input.substr(0,i));
+										                    auto right = diffWaysToCompute(input.substr(i+1));
+												                    
+												                    for(auto l:left){
+															                        for(auto r:right){
+																			                        if(input[i] == '+'){
+																							                            ans.push_back(l+r);
+																										                            }else if(input[i] == '-'){
+																														                                ans.push_back(l-r);
+																																		                        }else{
+																																						                            ans.push_back(l*r);
+																																									                            }
+																						                    }
+																		                }
+														                }
+							            }
+					        
+					        if(ans.empty()){
+							            ans.push_back(atoi(input.c_str()));
+								            }
+						        
+						        return ans;
+							    }
+	private:
+}
+
