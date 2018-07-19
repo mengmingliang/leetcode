5802cd885f330dd0123e04672c2f22433c36d2c7
mengmingliang <mml1106@126.com>
2018-07-19 12:52:58 +0800

    modify by mike meng

--- 518/solution.cpp
+++ 518/solution.cpp
@@ -32,3 +32,33 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int ans = 0;
+        vector<int> dp(amount+1,0);
+        
+        if(coins.size() < 0){
+            return 0;
+        }
+        
+        if(amount == 0){
+            return 1;
+        }
+        
+        sort(coins.begin(),coins.end());
+        for(int j = 0;j < coins.size(); ++j){
+            for(int i = 1;i <= amount; ++i){
+                    if(i == coins[j]){
+                        dp[i] = dp[i] + 1;
+                    }
+                    if(i > coins[j]){
+                        dp[i] = dp[i] + dp[i-coins[j]];
+                    }
+            }
+        }
+        
+        return dp[amount];
+    }
+};
