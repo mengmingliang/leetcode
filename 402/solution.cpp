12accb1af6c8aa6aa306f754fa392a434c35283e
mike-meng <mml1106@126.com>
2018-08-30 14:59:06 +0800

    add by mike meng

--- 402/solution.cpp
+++ 402/solution.cpp
@@ -1,66 +1,52 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char> s;
-        string ans;
+       stack<char> s;
+       string ans;
+       string pre;
+       int idx = 0;
         
-        if(k >= num.size()){
-            return "0";
-        }
+       if(k >= num.size()){
+           return "0";
+       }
         
-        if( k == 0){
-            return num;
-        }
+       while(k > 0 && idx < num.size()){
+           if(s.empty()){
+               s.push(num[idx++]);
+           }else{
+               if(s.top() > num[idx]){
+                   while(k > 0 && !s.empty() && s.top() > num[idx]){
+                       s.pop();
+                       k--;
+                   }   
+               }
+               s.push(num[idx++]);
+           }
+       }
+       
+       while(k> 0 && !s.empty()){
+           s.pop();
+           k--;
+       }
         
-        reverse(num.begin(),num.end());
-        cout<<"asd"<<endl;
+       while(!s.empty()){
+           ans.push_back(s.top());
+           s.pop();
+       } 
+       reverse(ans.begin(),ans.end());        
+       ans += num.substr(idx,num.size()-idx); 
         
-        for(int i = 0;i < num.size(); ++i){
-            if(k == 0){
-                for(int j = i;j < num.size(); ++j){
-                    s.push(num[j]);
-                }
-                break;
-            }
-            if(s.empty()){
-                s.push(num[i]);
-            }else{
-                if(num[i] > s.top()){
-                    k--;
-                    continue;
-                }else if(num[i] == s.top()){
-                    s.push(num[i]);
-                }else{
-                    while(s.top() > num[i] && k > 0 && !s.empty()){
-                        s.pop();
-                        k--;
-                    }
-                    if(k == 0){
-                        for(int j = i;j < num.size(); ++j){
-                            s.push(num[j]);
-                        }
-                        break;
-                    }else{
-                        s.push(num[i]);
-                    }
-                }
-            }
-        }
+       int zero = 0;
+       for(int i = 0;i < ans.size()-1; ++i){
+           if(ans[i] == '0'){
+               zero++;
+           }else{
+               break;
+           }
+       }
         
-        cout<<"asdds"<<endl;
+       ans = ans.substr(zero,ans.size()-zero);
         
-        if(k != 0){
-            while(k > 0 && !s.empty()){
-                s.pop();
-                k--;
-            }
-        }
-            
-        while(!s.empty()){
-            ans.push_back(s.top());
-            s.pop();
-        }
-        
-        return ans;
+       return ans;
     }
 };
